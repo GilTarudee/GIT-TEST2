c57836a3179f07066e166b7f064afa90f3b40d63
GilTarudee <tarudee.aut@gmail.com>
2025-06-23 23:02:01 +0900

    add main +page and main layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,35 @@
 <script lang="ts">
-    let count = 0;
-  </script>
+  import Button from '$lib/iu/Button.svelte';
+  import type {PageProps} from './$types';
+
+  let {data}: PageProps = $props();
+</script>
+
+<div class= "bg-sky-100 h-full w-full">
+
+<div class="h-65 grid place-content-center">
   
-  <h1>Counter: {count}</h1>
-  <button on:click={() => count++}>Increase</button>
-  <button on:click={() => count--}>Decrease</button>
-  <button on:click={() => count *= 2}>Double</button>
+  <h1 class="text-xl m-3 font-bold ">
+    My Applications
+  </h1>
+  
+  <Button class="shadow-lg shadow-sky-700/50 hover:bg-sky-700 text-center m-2 font-bold" href="/calculator_PBenz" id="calculator_app">
+    Calculator
+  </Button>
+  
+  <Button class="shadow-lg shadow-sky-700/50 hover:bg-sky-700 text-center m-2 font-bold" href="/todolist" id="To do lists app">
+    To do lists
+  </Button>
+
+  <Button class="shadow-lg shadow-sky-700/50 hover:bg-sky-700 text-center m-2 font-bold " href="/digimon" id="To do lists app">
+    Digimon Info
+  </Button>
+
+</div>
+
+<div class="text-gray-600 py-3 px-3 font-bold grid place-items-end">
+Wait for new apps..
+</div>
+
+</div>
+
